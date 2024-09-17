f06462a076532f87a3d2d909b29395756b179687
Nathan Garzya Santoso <85006633+canonflow@users.noreply.github.com>
2024-09-18 00:58:59 +0700

    Initial commit

---
+++ app/src/main/java/com/pemujaandroidstudio/esportcompanyprofileapp/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.pemujaandroidstudio.esportcompanyprofileapp
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.BindingAdapter
+import com.pemujaandroidstudio.esportcompanyprofileapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+}

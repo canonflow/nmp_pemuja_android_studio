93646bbffca3a8f332f99738c2bf1948a204faba
Nathan Garzya Santoso <85006633+canonflow@users.noreply.github.com>
2024-11-24 13:10:12 +0700

    ACC 3 DONE

--- app/src/main/java/com/pemujaandroidstudio/esportcompanyprofileapp/LoginActivity.kt
+++ app/src/main/java/com/pemujaandroidstudio/esportcompanyprofileapp/LoginActivity.kt
@@ -1,14 +1,20 @@
 package com.pemujaandroidstudio.esportcompanyprofileapp
 
+import android.app.AlertDialog
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.pemujaandroidstudio.esportcompanyprofileapp.databinding.ActivityLoginBinding
+import org.json.JSONObject
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding:ActivityLoginBinding
@@ -23,7 +29,7 @@ class LoginActivity : AppCompatActivity() {
         var username = sharedPreferences.getString("USERNAME",
             "").toString()
         if(!username.equals("")){
-            val intent = Intent(this, WhatWePlayActivity::class.java)
+            val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
         }
@@ -32,12 +38,46 @@ class LoginActivity : AppCompatActivity() {
             val password = binding.txtPasswordValue.text.toString() ?: ""
 
             if(!username.equals("") && !password.equals("")){
-                val editor = sharedPreferences.edit()
-                editor.putString("USERNAME", username)
-                editor.apply()
-                val intent = Intent(this, WhatWePlayActivity::class.java)
-                startActivity(intent)
-                finish()
+//                val editor = sharedPreferences.edit()
+//                editor.putString("USERNAME", username)
+//                editor.apply()
+//                val intent = Intent(this, MainActivity::class.java)
+//                startActivity(intent)
+//                finish()
+                val q = Volley.newRequestQueue(it.context)
+                val url = "https://ubaya.xyz/native/160422065/login.php"
+                var stringRequest = object : StringRequest(
+                    Request.Method.POST,
+                    url,
+                    {
+                        Log.d("cek", it)
+                        val obj = JSONObject(it)
+                        val result = obj.getString("result")
+                        val message = obj.getString("message")
+                        if (result.equals("OK", false)) {
+                            val editor = sharedPreferences.edit()
+                            editor.putString("USERNAME", username)
+                            editor.apply()
+                            showDialog("Success", "Login Success", true, MainActivity::class.java)
+                        } else {
+                            showDialog("Failed", message)
+                        }
+                    },
+                    {
+                        Log.d("cek", it.message.toString())
+                    }
+                )
+                {
+                    override fun getParams(): MutableMap<String, String> {
+                        val params = HashMap<String, String>()
+                        params["username"] = username
+                        params["password"] = password
+                        return params
+                    }
+                }
+                q.add(stringRequest)
+            } else {
+                showDialog("Failed", "Username or Password is empty")
             }
         }
 
@@ -48,5 +88,17 @@ class LoginActivity : AppCompatActivity() {
         }
     }
 
-
+    fun showDialog(title: String, message: String, callback: Boolean = false, activity: Class<out AppCompatActivity>? = null) {
+        AlertDialog.Builder(this)
+            .setTitle(title)
+            .setMessage(message)
+            .setPositiveButton("OK") { _, _ ->
+                if (callback && activity != null) {
+                    val intent = Intent(this, activity)
+                    startActivity(intent)
+                    finish()
+                }
+            }
+            .show()
+    }
 }
--- app/src/main/java/com/pemujaandroidstudio/esportcompanyprofileapp/SignUpActivity.kt
+++ app/src/main/java/com/pemujaandroidstudio/esportcompanyprofileapp/SignUpActivity.kt
@@ -1,13 +1,20 @@
 package com.pemujaandroidstudio.esportcompanyprofileapp
 
+import android.app.Activity
+import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.pemujaandroidstudio.esportcompanyprofileapp.databinding.ActivityLoginBinding
 import com.pemujaandroidstudio.esportcompanyprofileapp.databinding.ActivitySignUpBinding
+import org.json.JSONObject
 
 class SignUpActivity : AppCompatActivity() {
     private lateinit var binding:ActivitySignUpBinding
@@ -23,7 +30,39 @@ class SignUpActivity : AppCompatActivity() {
             val password = binding.txtPasswordSignUp.text.toString()
             val passwordrepeat = binding.txtPasswordRepeatSignUp.text.toString()
             if (password == passwordrepeat) {
-
+                val q = Volley.newRequestQueue(it.context)
+                val url = "https://ubaya.xyz/native/160422065/register.php"
+                var stringRequest = object : StringRequest(
+                    Request.Method.POST,
+                    url,
+                    {
+                        Log.d("cek", it)
+                        val obj = JSONObject(it)
+                        val result = obj.getString("result")
+                        val message = obj.getString("message")
+                        if (result.equals("OK", false)) {
+                            showDialog("Success", message, true, LoginActivity::class.java);
+                        } else {
+                            showDialog("Failed", message)
+                        }
+                    },
+                    {
+                        Log.d("cek", it.message.toString())
+                    }
+                )
+                {
+                    override fun getParams(): MutableMap<String, String> {
+                        val params = HashMap<String, String>()
+                        params["username"] = username
+                        params["password"] = password
+                        params["fname"] = firstname
+                        params["lname"] = lastname
+                        return params
+                    }
+                }
+                q.add(stringRequest)
+            } else {
+                showDialog("Failed", "Password doesn't match")
             }
         }
         binding.checkBoxAgree.setOnCheckedChangeListener {
@@ -35,5 +74,19 @@ class SignUpActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
+    }
+
+    fun showDialog(title: String, message: String, callback: Boolean = false, activity: Class<out AppCompatActivity>? = null) {
+        AlertDialog.Builder(this)
+            .setTitle(title)
+            .setMessage(message)
+            .setPositiveButton("OK") { _, _ ->
+                if (callback && activity != null) {
+                    val intent = Intent(this, activity)
+                    startActivity(intent)
+                    finish()
+                }
+            }
+            .show()
     }
 }
